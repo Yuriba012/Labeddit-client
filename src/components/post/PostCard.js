16fec353d2cec1163617ffa142f2d917f17ecee7
unknown <yuriba012@gmail.com>
2023-05-22 12:57:36 -0300

    API req

--- src/components/post/PostCard.js
+++ src/components/post/PostCard.js
@@ -1,30 +1,119 @@
-import { CommentsNumber, Container, LikesDiv, LikesNumber, OwnerName, PostContent, ReactionIcon, ReactionsDiv } from "./style"
-import likeIcon from '../../assets/likeIcon.png'
-import dislikeIcon from '../../assets/dislikeIcon.png'
-import commentIcon from '../../assets/commentIcon.png'
-import { useNavigate } from "react-router-dom"
-import { goToPostPage } from "../../routes/coordinator"
+import {
+  CommentsNumber,
+  Container,
+  LikesDiv,
+  LikesNumber,
+  OwnerName,
+  PostContent,
+  ReactionIcon,
+  ReactionsDiv,
+} from "./style";
+import likeIcon from "../../assets/likeIcon.png";
+import dislikeIcon from "../../assets/dislikeIcon.png";
+import commentIcon from "../../assets/commentIcon.png";
+import { useNavigate } from "react-router-dom";
+import { goToPostPage } from "../../routes/coordinator";
+import {
+  Button,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  EditableTextarea,
+  Flex,
+  IconButton,
+  InputGroup,
+  InputRightElement,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { BiEdit } from "react-icons/bi";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+import { useForm } from "../../hooks/useForm";
 
-export const PostCard = ({isComment}) =>{
-    const navigator = useNavigate()
-    return(
-        <Container>
-            <OwnerName>Enviado por: labaluno83 </OwnerName>
-            <PostContent>Porque a maioria dos desenvolvedores usam Linux? ou as empresas de tecnologia usam Linux ?</PostContent>
-            <ReactionsDiv>
-                <LikesDiv>
-                    <ReactionIcon src={likeIcon} />
-                    <LikesNumber>1.2k</LikesNumber>
-                    <ReactionIcon src={dislikeIcon} />
-                </LikesDiv>
-                { isComment === false?
-                <LikesDiv onClick={()=>goToPostPage(navigator, '1')}>
-                    <ReactionIcon src={commentIcon} />
-                    <CommentsNumber>163</CommentsNumber>
-                </LikesDiv>:
-                <></>
-                }
-            </ReactionsDiv>
-        </Container>
-    )
-}
+export const PostCard = ({ post, like, deleteCard, editContent }) => {
+  const { id, creator, content, likes, dislikes, comments } = post;
+  const [editing, setEditing] = useState(false);
+  const sendEdit = (e) =>{
+    e.preventDefault();
+    editContent(isComment, id, form.newContent);
+    setEditing(!editing);
+  }
+  const navigator = useNavigate();
+  const isComment = comments === undefined ? true : false;
+  const [form, onChangeInputs, clearInputs] = useForm({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <Container>
+      <Flex width="100%" direction="row" justify="space-between">
+        <OwnerName>Enviado por: {creator.name} </OwnerName>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<GiHamburgerMenu />}
+            variant="outline"
+            border="none"
+            height="12px"
+          />
+          <MenuList>
+            <MenuItem icon={<BiEdit />} onClick={() => setEditing(!editing)}>
+              Editar conteúdo
+            </MenuItem>
+            <MenuItem
+              icon={<RiDeleteBin6Line />}
+              onClick={() => deleteCard(isComment, id)}
+            >
+              Excluir
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+      {editing ? (
+        <form onSubmit={sendEdit} width='100%'>
+        <Flex direction="column" align="flex-start" gap="4px">
+          <Editable defaultValue={content}>
+            <EditablePreview bg="yellow.500" maxWidth="260px" />
+            <EditableInput
+              bg="yellow.500"
+              width="260px"
+              height="80px"
+              name="newContent"
+              onChange={onChangeInputs}
+            />
+          </Editable>
+          <Button type="submit">Enviar</Button>
+        </Flex>
+        </form>
+      ) : (
+        <PostContent>{content}</PostContent>
+      )}
+
+      <ReactionsDiv>
+        <LikesDiv>
+          <ReactionIcon
+            onClick={() => like(isComment, id, true)}
+            src={likeIcon}
+          />
+          <LikesNumber>{likes - dislikes}</LikesNumber>
+          <ReactionIcon
+            onClick={() => like(isComment, id, false)}
+            src={dislikeIcon}
+          />
+        </LikesDiv>
+        {!isComment ? (
+          <LikesDiv onClick={() => goToPostPage(navigator, id)}>
+            <ReactionIcon src={commentIcon} />
+            <CommentsNumber>{comments}</CommentsNumber>
+          </LikesDiv>
+        ) : undefined}
+      </ReactionsDiv>
+    </Container>
+  );
+};
--- src/pages/post/PostPage.js
+++ src/pages/post/PostPage.js
@@ -1,38 +1,137 @@
-import { Button, Flex, Input, Spacer, StackDivider, VStack } from "@chakra-ui/react"
-import { Header } from "../../components/header/Header"
-import { PostCard } from "../../components/post/PostCard"
+import { Button, Flex, StackDivider, Text, Textarea, VStack } from "@chakra-ui/react";
+import { Header } from "../../components/header/Header";
+import { PostCard } from "../../components/post/PostCard";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { createComment, getCommentsByPostId, getPostById } from "../../apiReq/apiReq";
+import { useForm } from "../../hooks/useForm";
+import { FormStyled } from "../feed/style";
+import { deleteItem, like, editItem } from "../../apiReq/cardFunctions";
+import { goToFeedPage } from "../../routes/coordinator";
+import { useNavigate } from "react-router-dom";
 
-export const PostPage
- = () => {
-    const isComment = true
-    const currentPage = 'post'
-    return(
-        <Flex direction='column' justify='flex-start' height='100vh'>
-        <Header currentPage={currentPage}/>
-        <Flex flex='1' direction='column' justify='space-between' padding='28px' gap='12px'>
-            <PostCard isComment={false}/>
-            <VStack gap="18px" divider={<StackDivider borderColor="orange.500"/>} >
-                <Flex direction='column' justify='flex-start' width='100%' gap='12px'>
-                        <Input
-                        fontSize="16px"
-                        height="131px"
-                        borderRadius='12px'
-                        placeholder="Adicionar comentário"
-                        bg='gray.500'
-                        _placeholder={{ color: "inherit", fontSize: "16px" }}
-                        ></Input>
-                    <Button variant='gradient'>Responder</Button>
-                </Flex>
-                <Flex direction='column' justify='flex-start' width='100%' gap='12px'>
-                    <PostCard isComment={isComment}/>
-                    <PostCard isComment={isComment}/>
-                    <PostCard isComment={isComment}/>
-                    <PostCard isComment={isComment}/>
-                    <PostCard isComment={isComment}/>
-                    <PostCard isComment={isComment}/>
-                </Flex>
-            </VStack>
-        </Flex>
-    </Flex>
-    )
+export const PostPage = () => {
+  const { id } = useParams();
+  const navigator = useNavigate()
+  const [post, setPost] = useState("");
+  const [commentsList, setCommentsList] = useState([]);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [successMessage, setSuccessMessage] = useState("");
+
+const likePostPage = async (isComment, id, value) => {
+  await like(isComment, id, value, setErrorMessage, setSuccessMessage)
 }
+
+const deletePostPageCard = async (isComment, id) => {
+  await deleteItem(isComment, id, setErrorMessage, setSuccessMessage)
+}
+
+const editContentPostPage = async (isComment, id, newContent) => {
+  await editItem(
+    isComment,
+    id,
+    newContent,
+    setErrorMessage,
+    setSuccessMessage
+  );
+};
+
+  const [form, onChangeInputs, clearInputs] = useForm({
+      content: ""
+  });
+
+  const requestPost = async (token, id) => {
+    try {
+      const response = await getPostById(token, id);
+      setPost(response);
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(error.response.data)
+      goToFeedPage(navigator)
+    }
+  };
+
+  const requestComments = async (token, id) => {
+
+    try {
+      const response = await getCommentsByPostId(token, id);
+      setCommentsList(response);
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(error.response.data)
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await createComment(localStorage.getItem("tokenLabeddit"), {
+        content: form.content,
+      }, id);
+      setSuccessMessage(response)
+      clearInputs();
+    } catch (error) {
+      console.log(error.response.data);
+      setErrorMessage(error.response.data);
+    }
+  };
+
+
+  useEffect(() => {
+    requestPost(localStorage.getItem("tokenLabeddit"), id);
+  }, [successMessage ,id, commentsList]);
+
+  useEffect(() => {
+    requestComments(localStorage.getItem("tokenLabeddit"), id);
+  }, [successMessage, id]);
+
+
+
+
+  const currentPage = "post";
+
+  return (
+    <Flex direction="column" justify="flex-start" height="100vh">
+      <Header currentPage={currentPage} />
+      <Flex
+        flex="1"
+        direction="column"
+        justify="flex-start"
+        padding="28px"
+        gap="12px"
+      >
+
+        {post !== ""?<PostCard post={post} like = {likePostPage} deleteCard = {deletePostPageCard} editContent={editContentPostPage} />:undefined}
+        {errorMessage ? (
+                <Text color="red">{errorMessage}</Text>
+              ) : undefined}
+        <VStack gap="18px" divider={<StackDivider borderColor="orange.500" />}>
+            <FormStyled onSubmit={onSubmit}>
+          <Flex direction="column" justify="flex-start" width="100%" gap="12px">
+            <Textarea
+            value={form.content}
+            type="text"
+            name="content"
+            onChange={onChangeInputs}
+              fontSize="16px"
+              height="131px"
+              borderRadius="12px"
+              placeholder="Adicionar comentário"
+              bg="gray.500"
+              _placeholder={{ color: "inherit", fontSize: "16px" }}
+            ></Textarea>
+            <Button type="submit" variant="gradient">Responder</Button>
+          </Flex>
+          </FormStyled>
+          <Flex direction="column" justify="flex-start" width="100%" gap="12px">
+            {commentsList.length !== 0?commentsList.map((comment) => (
+              <PostCard key={comment.id} post={comment}  like = {likePostPage} deleteCard={deletePostPageCard} editContent={editContentPostPage} />
+            )): undefined}
+          </Flex>
+        </VStack>
+      </Flex>
+    </Flex>
+  );
+};
